606353f8bf6d14d0587c3a67e271290270c67de7
bianavic <bianavic@gmail.com>
2023-07-07 15:30:28 -0300

    fix: calculateCurrencyConversion test

--- src/test/kotlin/com/currency/calculator/controller/ExchangeRateControllerTest.kt
+++ src/test/kotlin/com/currency/calculator/controller/ExchangeRateControllerTest.kt
@@ -1,14 +1,13 @@
 package com.currency.calculator.controller
 
 import com.currency.calculator.client.model.ConversionRatesResponse
+import com.currency.calculator.service.ExchangeRateService
 import com.github.tomakehurst.wiremock.WireMockServer
 import com.github.tomakehurst.wiremock.client.WireMock
 import com.google.gson.Gson
-import org.junit.jupiter.api.AfterEach
-import org.junit.jupiter.api.Assertions
-import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.DisplayName
-import org.junit.jupiter.api.Test
+import io.mockk.every
+import io.mockk.mockk
+import org.junit.jupiter.api.*
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
@@ -26,6 +25,9 @@ internal class ExchangeRateControllerTest {
     @Autowired
     lateinit var exchangeRateController: ExchangeRateController
 
+    @Autowired
+    lateinit var exchangeRateService: ExchangeRateService
+
     var wireMockServer = WireMockServer(9999)
 
     @BeforeEach
@@ -41,6 +43,7 @@ internal class ExchangeRateControllerTest {
     @Test
     @DisplayName("should get latest rates for BRL money")
     fun getLatestRatesFor() {
+
         var resultExpected = ConversionRatesResponse(1.0, 0.1892, 17.0002, 0.2061)
         val baseCode = "BRL"
 
@@ -56,27 +59,28 @@ internal class ExchangeRateControllerTest {
         Assertions.assertEquals(result, resultExpected)
     }
 
-//    @Test
-//    fun `should calculate currency conversion`() {
-//        // Arrange
-//        val amount = 529.99
-//        val baseCode = "BRL"
-//        val conversionRates = ConversionRatesResponse(1.0, 5.278, 16.90660, 4.85)
-//        every { exchangeRateService.getLatestByBaseCode(baseCode) } returns conversionRates
-//
-//        // Act
-//        val result = mockMvc.perform(MockMvcRequestBuilders.get("/calculate/{amount}", amount))
-//            .andExpect(MockMvcResultMatchers.status().isOk)
-//            .andReturn()
-//
-//        // Assert
-//        val responseType = object : TypeToken<Map<String, Double>>() {}.type
-//        val response = gson.fromJson<Map<String, Double>>(result.response.contentAsString, responseType)
-//        response shouldBe mapOf(
-//            "EUR" to (amount * conversionRates.EUR),
-//            "USD" to (amount * conversionRates.USD),
-//            "INR" to (amount * conversionRates.INR)
-//        )
-//    }
+    @Test
+    @DisplayName("should calculate currency conversion")
+    fun calculateCurrencyConversion() {
+
+        val amount = 529.99
+        val baseCode = "BRL"
+        val conversionRates = ConversionRatesResponse(1.0, 5.00, 16.00, 4.00)
+
+        val expectedResponse = mapOf(
+            "EUR" to (amount * conversionRates.EUR),
+            "USD" to (amount * conversionRates.USD),
+            "INR" to (amount * conversionRates.INR)
+        )
+
+        val exchangeRateService = mockk<ExchangeRateService>()
+        every { exchangeRateService.getLatestByBaseCode(baseCode) } returns conversionRates
+
+        val exchangeRateController = ExchangeRateController(exchangeRateService)
+
+        val result = exchangeRateController.calculateCurrencyConversion(amount)
+
+        Assertions.assertEquals(expectedResponse, result)
+    }
 
 }
