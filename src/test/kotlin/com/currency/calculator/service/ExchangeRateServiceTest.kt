471fc0b8b91a6aa37b36bfb2a7df9c7b877ac572
bianavic <bianavic@gmail.com>
2023-07-21 11:55:14 -0300

    test: update test after latest change

--- src/test/kotlin/com/currency/calculator/service/ExchangeRateServiceTest.kt
+++ src/test/kotlin/com/currency/calculator/service/ExchangeRateServiceTest.kt
@@ -28,71 +28,75 @@ class ExchangeRateServiceTest {
     @MockBean
     private lateinit var exchangeRateService: ExchangeRateService
 
-//    @Test
-//    fun `should return latest rates for a valid base code`() {
-//        // Arrange
-//        val baseCode = "BRL"
-//        val expectedConversionRates = RatesResponse(1.0, 5.0, 16.0, 4.0)
-//        val response = """
-//            {
-//                "base_code": "BRL",
-//                "conversion_rates": {
-//                    "BRL": 1.0,
-//                    "EUR": 5.0,
-//                    "USD": 4.0,
-//                    "INR": 16.0
-//                }
-//            }
-//        """
-//
-//        val exchangeFeignClient = mockk<ExchangeFeignClient>()
-//        every { exchangeFeignClient.getLatestExchangeFor(baseCode) } returns response
-//
-//        val exchangeRateService = ExchangeRateServiceImpl(exchangeFeignClient)
-//
-//        // Act
-//        val result = exchangeRateService.getLatestByBaseCode(baseCode)
-//
-//        // Assert
-//        assertEquals(expectedConversionRates, result)
-//    }
+    @Test
+    fun `should return latest rates for a valid base code`() {
+        // Arrange
+        val baseCode = "BRL"
+        val expectedConversionRates = RatesResponse(1.0, 5.0, 16.0, 4.0)
+        val response = """
+            {
+                "base_code": "BRL",
+                "conversion_rates": {
+                    "BRL": 1.0,
+                    "EUR": 5.0,
+                    "USD": 4.0,
+                    "INR": 16.0
+                }
+            }
+        """
 
-//    @Test
-//    fun `should throw BaseCodeNotFoundException when base code is not found` () {
-//        val exchangeFeignClient = mockk<ExchangeFeignClient>()
-//        val exchangeRateService = ExchangeRateServiceImpl(exchangeFeignClient)
-//
-//        val baseCode = "INVALID_BASE_CODE"
-//        every { exchangeFeignClient.getLatestExchangeFor(baseCode) } throws Exception("Base code not found")
-//
-//        assertThrows<BaseCodeNotFoundException> {
-//            exchangeRateService.getLatestByBaseCode(baseCode)
-//        }
-//    }
+        val exchangeFeignClient = mockk<ExchangeFeignClient>()
+        every { exchangeFeignClient.getLatestExchangeFor(any(), baseCode) } returns response
 
-//    @Test
-//    fun `should calculate currency conversion`() {
-//        // Arrange
-//        val amount = 529.99
-//        val exchangeFeignClient = mockk<ExchangeFeignClient>()
-//        val exchangeRateService = ExchangeRateServiceImpl(exchangeFeignClient)
-//
-//        // Stub getLatestByBaseCode
-//        val ratesResponseMock = RatesResponseMock()
-//        val rates = ratesResponseMock.getLatestRates()
-//        every { exchangeFeignClient.getLatestExchangeFor("BRL") } returns getMockApiResponse(rates)
-//
-//        // Act
-//        val result = exchangeRateService.calculate(amount)
-//
-//        // Assert
-//        val expectedResponse = mapOf(
-//            "EUR" to (amount * rates.EUR),
-//            "USD" to (amount * rates.USD),
-//            "INR" to (amount * rates.INR)
-//        )
-//        assertEquals(expectedResponse, result)
-//    }
+        val exchangeApiUrl = "https://v6.exchangerate-api.com/v6/\${EXCHANGE_API_KEY}"
+
+        val exchangeRateService = ExchangeRateServiceImpl(exchangeFeignClient, exchangeApiUrl)
+
+        // Act
+        val result = exchangeRateService.getLatestByBaseCode(baseCode)
+
+        // Assert
+        assertEquals(expectedConversionRates, result)
+    }
+
+    @Test
+    fun `should throw BaseCodeNotFoundException when base code is not found` () {
+        val exchangeFeignClient = mockk<ExchangeFeignClient>()
+        val exchangeApiUrl = "https://v6.exchangerate-api.com/v6/\${EXCHANGE_API_KEY}"
+        val exchangeRateService = ExchangeRateServiceImpl(exchangeFeignClient, exchangeApiUrl)
+
+        val baseCode = "INVALID_BASE_CODE"
+        every { exchangeFeignClient.getLatestExchangeFor(any(), baseCode) } throws Exception("Base code not found")
+
+        assertThrows<BaseCodeNotFoundException> {
+            exchangeRateService.getLatestByBaseCode(baseCode)
+        }
+    }
+
+    @Test
+    fun `should calculate currency conversion`() {
+        // Arrange
+        val amount = 529.99
+        val exchangeFeignClient = mockk<ExchangeFeignClient>()
+        val exchangeApiUrl = "https://v6.exchangerate-api.com/v6/\${EXCHANGE_API_KEY}"
+        val exchangeRateService = ExchangeRateServiceImpl(exchangeFeignClient, exchangeApiUrl)
+
+        // Stub getLatestByBaseCode
+        val ratesResponseMock = RatesResponseMock()
+        val rates = ratesResponseMock.getLatestRates()
+        every { exchangeFeignClient.getLatestExchangeFor(any(),"BRL") } returns getMockApiResponse(rates)
+
+        // Act
+        val result = exchangeRateService.calculate(amount)
+
+        // Assert
+        val expectedResponse = mapOf(
+            "EUR" to (amount * rates.EUR),
+            "USD" to (amount * rates.USD),
+            "INR" to (amount * rates.INR)
+        )
+        assertEquals(expectedResponse, result)
+    }
 
     @Test
     fun `should throw MalformedRequestException when amount is invalid`() {
